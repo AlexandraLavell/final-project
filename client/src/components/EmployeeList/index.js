9d3d9c00d1ab9db6ed41aa6155fb55e8c6095050
Alexandra Lavell <alexandra.lavell@hushmail.com>
2021-12-03 15:06:28 -0500

    update 3 306

---
+++ client/src/components/EmployeeList/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+
+const EmployeeList = () => {
+
+
+
+
+    // start of main return
+    return (
+
+        <p>EmployeeList</p>
+
+
+
+
+
+    ) // end of main return
+
+
+
+
+
+}
+
+export default EmployeeList;
---
+++ client/src/components/ProjectCard/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+
+const ProjectCard = () => {
+
+
+
+
+    // start of main return
+    return (
+
+        <p>ProjectCard</p>
+
+
+
+
+
+    ) // end of main return
+
+
+
+
+
+}
+
+export default ProjectCard;
