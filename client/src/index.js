b8368ec374ed9bfe4fcf4b36ca099067a9b9f187
Alexandra Lavell <alexandra.lavell@hushmail.com>
2021-12-03 12:03:03 -0500

    update folders

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// main App component
+import App from './App';
+
+// main render
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
