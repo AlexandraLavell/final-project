61d9e66381389eec4ef458c2f1f58fc8409986cb
Alexandra Lavell <alexandra.lavell@hushmail.com>
2021-12-06 09:56:22 -0500

    moving cards

--- client/src/components/EmployeeList/index.js
+++ client/src/components/EmployeeList/index.js
@@ -4,19 +4,36 @@ import { EmployeeListWrapper } from "./StyledEmployeeList";
 
 
 
-const EmployeeList = () => {
+const EmployeeList = (props) => {
 
+    // 
+    const drop = (ev) => {        
+        ev.preventDefault();
 
+        const card_id = ev.dataTransfer.getData("card_id")
 
+        const card= document.getElementById(card_id);
+
+        card.style.display = "block";
+
+        ev.target.appendChild(card);
+
+    }
+
+    const dragOver = (ev) => {
+        // allows the card to appear on drop. Default is to disappear.
+        ev.preventDefault();
+    }
 
     // start of main return
     return (
-        <EmployeeListWrapper>
-            
-
-            <p>EmployeeList</p>
-
-
+        <EmployeeListWrapper
+                    id={props.id}
+                    onDrop={drop}
+                    onDragOver={dragOver}
+                    className={props.className}
+                    >
+            { props.children }
         </EmployeeListWrapper>
 
 
