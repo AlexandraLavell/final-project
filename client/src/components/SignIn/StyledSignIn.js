d0fedf5726eb6e032ca94a2555f7526d898ed1f1
Alexandra Lavell <alexandra.lavell@hushmail.com>
2021-12-07 13:56:41 -0500

    update 127 1356

--- client/src/components/SignIn/StyledSignIn.js
+++ client/src/components/SignIn/StyledSignIn.js
@@ -5,15 +5,30 @@ import styled from "styled-components";
 
 
 export const SignInWrapper = styled.div`
-    position: absolute;
+    position: relative;
     display: flex;
     flex-direction: column;
-    height: 100vh;
-    width: 100vw;
+    height: 99vh;
+    width: 100%;
+    padding: 5px;
     background-color: var(--color-main-yellow);
 
 `;
 
-export const Greeting = styled.div``;
+export const Greeting = styled.div`
+    font-family: "Courier New";
+    font-size: 256px;
 
-export const GoToButton = styled(NavLink) ``;
+`;
+
+export const GoToButton = styled(NavLink) `
+    height: 1.2em;
+    padding: 3px;
+    background-color: black;
+    position: absolute;
+    bottom: 5px;
+    right: 5px;
+    font-size: .2em;
+    text-decoration: none;
+    color: var(--color-main-yellow);
+`;
