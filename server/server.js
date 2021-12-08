8954d0784fe3bb9acc38ae18d4ce8edba27d4d17
Alexandra Lavell <alexandra.lavell@hushmail.com>
2021-12-08 08:43:38 -0500

    last update tuesday 128 843

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ const app = express()
 // import handlers
 
 
-
 // to log more info in the console
 app.use(morgan("tiny"));
 app.use(express.json());
@@ -19,26 +18,42 @@ app.use(express.json());
 // requests for static files will be directed to the public folder
 app.use(express.static("public"));
 
+// example
+// .patch("/reservations/update", updateReservation)
 
 
 // add a project
+app.post("/projects", addProject);
 // modify a project
-// find a project by id/name
-// find a project by worker
+app.patch("/projects/:id", modifyProject);
+// find a project by id
+app.get("/projects/:id", findProjectById);
+// find a project by employee
+app.get("/projects/employee/:id", findProjectByEmployee);
 // find all projects
+app.get("/projects", getAllProjects);
 // delete a project
+app.delete("/projects/:id", deleteProjectById);
 // delete all projects
-// add a worker
-// modify a worker
-// find a worker by id/name
-// find a worker by project
-// find all workers
-// delete a worker
-// delete all workers
+app.delete("/projects", deleteAllProjects);
+// add a employee
+app.post("employees", addEmployee);
+// modify a employee
+app.patch("/employees/:id", modifyEmployee);
+// find an employee by id/name
+app.get("/employee/:id", findEmployeeById);
+// find an employee by project
+app.get("/employees/project/:id");
+// find all employees
+app.get("/employees", getAllEmployees);
+// delete a employee
+app.delete("/employees/:id", deleteEmployee);
+// delete all employees
+app.delete("/employees", deleteAllEmployees);
 // add a calendar item
 // modify a calendar item
 // delete a calendar item
-// find calandar items by worker
+// find calandar items by employee
 // find calendar item by project
 // find all calendar items
 
