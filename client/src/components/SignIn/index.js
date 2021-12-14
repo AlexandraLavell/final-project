8a9f0dc14eaecc4a9430978b678852cd07dc3140
Alexandra Lavell <alexandra.lavell@hushmail.com>
2021-12-14 10:11:50 -0500

    signin 1214 1011

--- client/src/components/SignIn/index.js
+++ client/src/components/SignIn/index.js
@@ -3,7 +3,10 @@ import React, { useContext } from "react";
 // styled components
 import {    SignInWrapper,
             Greeting,
-            GoToButton  } from "./StyledSignIn";
+            GoToButton,
+            SignInForm,
+            FormInput,
+            FormLabel  } from "./StyledSignIn";
 
 // context
 import MainContext from "../MainContext";
@@ -11,7 +14,13 @@ import MainContext from "../MainContext";
 const SignIn = () => {
 
     // consume context
-    const { setSignInPage } = useContext(MainContext);
+    const { signInPage, setSignInPage,
+            setPermission,
+            } = useContext(MainContext);
+
+    const handleSubmit = (ev) => {
+        setPermission(true);
+    }
 
     // main return
     return (
@@ -19,7 +28,23 @@ const SignIn = () => {
             <Greeting>
                 <p>Good</p>
                 <p>Morning.</p>
-                <GoToButton exact to="/dash" onClick={() => setSignInPage(false)}>ready</GoToButton>
+                {!signInPage && <GoToButton type="button" 
+                            onClick={() => setSignInPage(true)}
+                            value="ready"
+                            className="pointer"
+                />}
+                {signInPage && <SignInForm onSubmit={handleSubmit}>
+                    <FormLabel><p>user</p><p>name</p>
+                        <FormInput type="text" />
+                    </FormLabel>
+                    <FormLabel><p>pass</p><p>word</p>
+                        <FormInput type="text" />
+                    </FormLabel>
+                    <GoToButton type="submit"                             
+                                value="go"
+                                className="pointer go"
+                    />
+                </SignInForm>}
             </Greeting>
         </SignInWrapper>
     ) // end of main return
